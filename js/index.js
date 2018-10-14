49854e4af9bd2d77196762abf8d8501f291517b4
Ethan Le <ethan.le@innovatube.com>
2018-10-14 21:36:16 +0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function renderImage(file, target){
+  var reader = new FileReader();
+  reader.onload = function(event){
+    url = event.target.result
+    target.attr("src", url);
+  }
+  reader.readAsDataURL(file);
+}
+$("#designer_file").change(function(){
+  renderImage(this.files[0], $("#image_1"));
+  renderImage(this.files[1], $("#image_2"));
+  console.log("file was selected");
+  
+})
+
+$("#opacityRange").on("input", function(){
+  $("#image_2").fadeTo(0, this.value/100);
+})
