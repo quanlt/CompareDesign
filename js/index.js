dda696ef43650010c8da030f1b9ae62869995dd4
quanlt <quanlt.56@gmail.com>
2018-10-16 17:40:21 +0700

    :tada: update design and allow change mode at runtime

--- js/index.js
+++ js/index.js
@@ -14,6 +14,37 @@ $("#designer_file").change(function(){
 })
 
 $("#opacityRange").on("input", function(){
-  console.log("input " + this.value);
-  $("#image_1").fadeTo(0, this.value/100);
+  $("#opacity").html(this.value+"%");
+  $("#image_2").fadeTo(0, this.value/100);
 })
+
+function toggleCompareMode(){
+  $("#overlay").toggleClass("disabled");
+  $("#split").toggleClass("disabled");
+  $("#opacityRange").prop("disabled",!$("#overlay").hasClass("disabled"));
+  $("#preview").toggleClass("cocoen");
+  $("#image_1").toggleClass("actual");
+  $("#image_2").toggleClass("expected");
+  $(".cocoen-drag").toggle();
+  if($("#overlay").hasClass("disabled")){
+    $("#image_2").fadeTo(0, $("#opacityRange").val()/100);
+  } else {
+    $("#image_2").fadeTo(0, 1);
+  }
+}
+function updateImageSize(){
+  $('#image_2').css({'width' :$("#width").val() + "px" , 'height' : "'" +$("#height").val() + "px"});
+  $('#image_1').css({'width' :$("#width").val() + "px" , 'height' : "'" +$("#height").val() + "px"});
+}
+
+$("#overlay").click(toggleCompareMode);
+$("#split").click(toggleCompareMode);
+$("#overwriteDimenion").change(function(){
+  $("#submitSize").toggleClass("disabled");
+  if(this.checked){
+    updateImageSize();
+  }
+})
+$("#submitSize").click(function(){
+  updateImageSize();
+});
