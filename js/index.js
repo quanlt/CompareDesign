88c3ad86b9f13565f9e5f92fd5f3391759c6bd74
quanlt <quanlt.56@gmail.com>
2018-10-17 18:03:19 +0700

    :+1: add save function

--- js/index.js
+++ js/index.js
@@ -52,3 +52,138 @@ $("#overwriteDimenion").change(function(){
 $("#submitSize").click(function(){
   updateImageSize();
 });
+
+function createDownloadUri(uri, name) {
+  var link = document.createElement("a");
+
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+}
+
+// MIT http://rem.mit-license.org
+
+function trim(c) {
+  var ctx = c.getContext('2d'),
+    copy = document.createElement('canvas').getContext('2d'),
+    pixels = ctx.getImageData(0, 0, c.width, c.height),
+    l = pixels.data.length,
+    i,
+    bound = {
+      top: null,
+      left: null,
+      right: null,
+      bottom: null
+    },
+    x, y;
+
+  for (i = 0; i < l; i += 4) {
+    if (pixels.data[i+3] !== 0) {
+      x = (i / 4) % c.width;
+      y = ~~((i / 4) / c.width);
+  
+      if (bound.top === null) {
+        bound.top = y;
+      }
+      
+      if (bound.left === null) {
+        bound.left = x; 
+      } else if (x < bound.left) {
+        bound.left = x;
+      }
+      
+      if (bound.right === null) {
+        bound.right = x; 
+      } else if (bound.right < x) {
+        bound.right = x;
+      }
+      
+      if (bound.bottom === null) {
+        bound.bottom = y;
+      } else if (bound.bottom < y) {
+        bound.bottom = y;
+      }
+    }
+  }
+    
+  var trimHeight = bound.bottom - bound.top,
+      trimWidth = bound.right - bound.left,
+      trimmed = ctx.getImageData(bound.left, bound.top, trimWidth, trimHeight);
+  
+  copy.canvas.width = trimWidth;
+  copy.canvas.height = trimHeight;
+  copy.putImageData(trimmed, 0, 0);
+  
+  // open new window with trimmed image:
+  return copy.canvas;
+}
+
+function trimCanvas(c) {
+  var ctx = c.getContext('2d'),
+      copy = document.createElement('canvas').getContext('2d'),
+      pixels = ctx.getImageData(0, 0, c.width, c.height),
+      l = pixels.data.length,
+      i,
+      bound = {
+          top: null,
+          left: null,
+          right: null,
+          bottom: null
+      },
+      x, y;
+  
+  // Iterate over every pixel to find the highest
+  // and where it ends on every axis ()
+  for (i = 0; i < l; i += 4) {
+      if (pixels.data[i + 3] !== 0) {
+          x = (i / 4) % c.width;
+          y = ~~((i / 4) / c.width);
+
+          if (bound.top === null) {
+              bound.top = y;
+          }
+
+          if (bound.left === null) {
+              bound.left = x;
+          } else if (x < bound.left) {
+              bound.left = x;
+          }
+
+          if (bound.right === null) {
+              bound.right = x;
+          } else if (bound.right < x) {
+              bound.right = x;
+          }
+
+          if (bound.bottom === null) {
+              bound.bottom = y;
+          } else if (bound.bottom < y) {
+              bound.bottom = y;
+          }
+      }
+  }
+  
+  // Calculate the height and width of the content
+  var trimHeight = bound.bottom - bound.top,
+      trimWidth = bound.right - bound.left,
+      trimmed = ctx.getImageData(bound.left, bound.top, trimWidth, trimHeight);
+
+  copy.canvas.width = trimWidth;
+  copy.canvas.height = trimHeight;
+  copy.putImageData(trimmed, 0, 0);
+
+  // Return trimmed canvas
+  return copy.canvas;
+}
+
+
+$("#export").click(function() { 
+  html2canvas($("#preview"), {
+      onrendered: function(canvas) {
+        var trimmed = trimCanvas(canvas);
+        createDownloadUri(trimmed.toDataURL(), "compare_design.png");
+      }
+  });
+});
